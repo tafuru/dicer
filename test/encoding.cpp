c5fb7079bf009c4a70c75e38a0ecc2e4f57a9fbb
Takahiro Furukawa <tafuru@sugarcoat.cc>
2024-06-25 14:26:18 +0000

    refactor: Separate tests for ZigZag encoding and decoding

--- test/encoding.cpp
+++ test/encoding.cpp
@@ -8,24 +8,52 @@
 #include <type_traits>
 #include <vector>
 
-TEST_CASE_TEMPLATE("ZigZag Encoding", Int, std::int8_t, std::int16_t, std::int32_t, std::int64_t) {
+TEST_SUITE("ZigZag Encoding") {
+  TEST_CASE_TEMPLATE("Encodes Zero", Int, std::int8_t, std::int16_t, std::int32_t, std::int64_t) {
+    CHECK(dicer::EncodeZigZag<Int>(0) == 0U);
+  }
+
+  TEST_CASE_TEMPLATE("Encodes Positive Numbers", Int, std::int8_t, std::int16_t, std::int32_t, std::int64_t) {
+    CHECK(dicer::EncodeZigZag<Int>(1) == 2U);
+    CHECK(dicer::EncodeZigZag<Int>(2) == 4U);
+    CHECK(dicer::EncodeZigZag<Int>(3) == 6U);
+  }
+
+  TEST_CASE_TEMPLATE("Encodes Negative Numbers", Int, std::int8_t, std::int16_t, std::int32_t, std::int64_t) {
+    CHECK(dicer::EncodeZigZag<Int>(-1) == 1U);
+    CHECK(dicer::EncodeZigZag<Int>(-2) == 3U);
+    CHECK(dicer::EncodeZigZag<Int>(-3) == 5U);
+  }
+
+  TEST_CASE_TEMPLATE("Encodes Extreme Cases", Int, std::int8_t, std::int16_t, std::int32_t, std::int64_t) {
   using UInt = std::make_unsigned_t<Int>;
-  CHECK(dicer::EncodeZigZag(static_cast<Int>(0)) == static_cast<UInt>(0U));
-  CHECK(dicer::EncodeZigZag(static_cast<Int>(-1)) == static_cast<UInt>(1U));
-  CHECK(dicer::EncodeZigZag(static_cast<Int>(1)) == static_cast<UInt>(2U));
-  CHECK(dicer::EncodeZigZag(static_cast<Int>(-2)) == static_cast<UInt>(3U));
   CHECK(dicer::EncodeZigZag(std::numeric_limits<Int>::max()) == std::numeric_limits<UInt>::max() - 1U);
   CHECK(dicer::EncodeZigZag(std::numeric_limits<Int>::min()) == std::numeric_limits<UInt>::max());
 }
+}
 
-TEST_CASE_TEMPLATE("ZigZag Decoding", UInt, std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t) {
+TEST_SUITE("ZigZag Decoding") {
+  TEST_CASE_TEMPLATE("Decodes Zero", UInt, std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t) {
+    CHECK(dicer::DecodeZigZag<UInt>(0U) == 0);
+  }
+
+  TEST_CASE_TEMPLATE("Decodes Odd Numbers", UInt, std::uint8_t, std::uint16_t, std::uint32_t, std::uint32_t) {
+    CHECK(dicer::DecodeZigZag<UInt>(1U) == -1);
+    CHECK(dicer::DecodeZigZag<UInt>(3U) == -2);
+    CHECK(dicer::DecodeZigZag<UInt>(5U) == -3);
+  }
+
+  TEST_CASE_TEMPLATE("Decodes Even Numbers", UInt, std::uint8_t, std::uint16_t, std::uint32_t, std::uint32_t) {
+    CHECK(dicer::DecodeZigZag<UInt>(2U) == 1);
+    CHECK(dicer::DecodeZigZag<UInt>(4U) == 2);
+    CHECK(dicer::DecodeZigZag<UInt>(6U) == 3);
+  }
+
+  TEST_CASE_TEMPLATE("Decodes Extreme Cases", UInt, std::uint8_t, std::uint16_t, std::uint32_t, std::uint32_t) {
   using Int = std::make_signed_t<UInt>;
-  CHECK(dicer::DecodeZigZag(static_cast<UInt>(0U)) == static_cast<Int>(0));
-  CHECK(dicer::DecodeZigZag(static_cast<UInt>(1U)) == static_cast<Int>(-1));
-  CHECK(dicer::DecodeZigZag(static_cast<UInt>(2U)) == static_cast<Int>(1));
-  CHECK(dicer::DecodeZigZag(static_cast<UInt>(3U)) == static_cast<Int>(-2));
   CHECK(dicer::DecodeZigZag(std::numeric_limits<UInt>::max() - 1U) == std::numeric_limits<Int>::max());
   CHECK(dicer::DecodeZigZag(std::numeric_limits<UInt>::max()) == std::numeric_limits<Int>::min());
+  }
 }
 
 TEST_SUITE("Varint Encoding") {
